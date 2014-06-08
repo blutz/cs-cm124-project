52664fc4609a2bcdc9e9637f0dba631ad85b9957
Byron Lutz <byronlutz@gmail.com>
2014-06-08 00:45:51 -0700

    Fixed a few bugs with min_insertions

--- improved/mapper.py
+++ improved/mapper.py
@@ -14,6 +14,8 @@ SUFFIX_TREE_SIZE = 10
 READ_LENGTH = 50
 READ_DIVISIONS = READ_LENGTH/SUFFIX_TREE_SIZE
 
+DEBUG=True
+
 # Returns str/pos inserts needed to make fragment equal to the
 # start of text
 def min_insertions(fragment, text, end=False):
@@ -28,17 +30,17 @@ def min_insertions(fragment, text, end=False):
     inserts_cur = 0
     inserts_len = 0
     while i < len(fragment):
-        if (i+inserts_len) >= len(text):
+        if (i-inserts_len) >= len(text):
             return([], False)
         # The boring case -- the text matches
-        if text[i+inserts_len] == fragment[i]:
+        if text[i-inserts_len] == fragment[i]:
             if len(inserts) > inserts_cur:
                 inserts_cur = len(inserts)
         else:
             # We need to insert a new placeholder into inserts
             if len(inserts) <= inserts_cur:
                 inserts.append({"pos":i, "str":""})
-            inserts[inserts_cur]['str'] += text[i+inserts_len]
+            inserts[inserts_cur]['str'] += fragment[i]
             inserts_len += 1
         i += 1
 
@@ -57,15 +59,23 @@ def min_insertions(fragment, text, end=False):
 # Returns a list of dictionaries with entries "pos" (where the insertion is) and "str"
 # (what the insertion is)
 def find_insertions(read, reference_genome, stree):
+    print read
     ret_size = 10
     ret_val = []
     # We're going to go through each of the five sections of the read
     # and look them up in the suffix tree until we get a match
-    for i in range(READ_DIVISIONS - 1):
+    for i in range(READ_DIVISIONS):
+        print read[i*SUFFIX_TREE_SIZE:(i+1)*SUFFIX_TREE_SIZE]
         suffixes = stree[read[i*SUFFIX_TREE_SIZE:(i+1)*SUFFIX_TREE_SIZE]]
         for suffix in suffixes:
+            print suffix
             inserts_left, valid_left = min_insertions(read[:i*SUFFIX_TREE_SIZE], reference_genome[:suffix], True)
+            print inserts_left
+            print valid_left
             inserts_right, valid_right = min_insertions(read[(i+1)*SUFFIX_TREE_SIZE:], reference_genome[suffix+SUFFIX_TREE_SIZE:], False)
+            print inserts_right
+            print valid_right
+            print "-----"
             if (((len(inserts_left) + len(inserts_right)) <= 2) and valid_left and
                 valid_right and ((len(inserts_left) + len(inserts_right) < ret_size) or
                 ret_val == None)):
@@ -73,6 +83,7 @@ def find_insertions(read, reference_genome, stree):
                     ins["pos"] += i
                 for ins in inserts_right:
                     ins["pos"] += i
+        print "****"
 
     return ret_val
 
@@ -101,6 +112,10 @@ def build_suffix_tree(genome):
     # Now actually put data in the tree
     for i in range(len(genome)-1-SUFFIX_TREE_SIZE):
         stree[genome[i:i+SUFFIX_TREE_SIZE]].append(i)
+    if DEBUG and False:
+        for a, b in stree.iteritems():
+            print str(a) + ": "
+            print " " + str(b)
     return stree
 
 # Main setup to read in the files
@@ -113,7 +128,8 @@ def main():
     chromosome_number = 0
     reference_genome = ""
     
-    print str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1048576) + "MB"
+    if DEBUG:
+        print str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1048576) + "MB"
 
     # These will throw errors if the file doesn't exist. Good enough for the baseline
     with open(sys.argv[1], "r") as ref_file:
@@ -134,12 +150,14 @@ def main():
 
     d2 = datetime.datetime.now()
     stree = build_suffix_tree(reference_genome)
-    print str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1048576) + "MB"
+    if DEBUG:
+        print str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1048576) + "MB"
     d3 = datetime.datetime.now()
 
     # Now reference_genome is the genome and reads is an array of all reads
 
-    print str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1048576) + "MB"
+    if DEBUG:
+        print str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1048576) + "MB"
     reads = []
     with open(sys.argv[2], "r") as reads_file:
         for line in reads_file:
@@ -150,19 +168,22 @@ def main():
 
     d4 = datetime.datetime.now()
     di = 0
-    print str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1048576) + "MB"
+    if DEBUG:
+        print str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1048576) + "MB"
     insertions = []
     for read in reads:
         read_insertions = find_insertions(read, reference_genome, stree)
-        print str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1048576) + "MB"
+        if DEBUG:
+            print str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1048576) + "MB"
         d5 = datetime.datetime.now()
-        if di >= 500:
+        if di >= 500 and DEBUG:
             print d4-d1
             print d5-d4
             sys.exit()
         if read_insertions:
             insertions.extend(read_insertions)
-        print insertions
+        if DEBUG:
+            print insertions
         di += 1
 
     for ins in sorted(insertions, key=lambda ins: ins["pos"]):
